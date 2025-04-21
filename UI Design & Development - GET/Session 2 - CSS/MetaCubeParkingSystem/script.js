bbafb9247389f56d7b3dd7d630e5bbc3785927ab
Kunal Upadhyay <kunal.upadhyay@metacube.com>
2025-04-21 14:38:43 +0530

    Adding the assignment 2

--- UI Design & Development - GET/Session 2 - CSS/MetaCubeParkingSystem/script.js
+++ UI Design & Development - GET/Session 2 - CSS/MetaCubeParkingSystem/script.js
@@ -8,6 +8,7 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    console.log(content);
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
@@ -15,3 +16,7 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+
+
