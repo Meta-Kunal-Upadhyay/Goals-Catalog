e0717d0d6636ac6a70e9906fc34ab7ab6c7e4661
Kunal Upadhyay <kunal.upadhyay@metacube.com>
2025-04-30 21:40:50 +0530

    Added the js assignment

---
+++ UI Design & Development - GET/Session 6 - JavaScript/MetaCubeParkingSystem/script.js
@@ -0,0 +1,22 @@
+
+// alert("Welcome To MetaCube Parking System");
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log(content);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+
