60a383adc9b1869df12627e6013738e53c928f16
Kunal Upadhyay <kunal.upadhyay@metacube.com>
2025-05-14 13:15:37 +0530

    Adding the files

--- UI Design & Development - GET/Session 10 - React - Session 1/task-manager/src/main.jsx
+++ UI Design & Development - GET/Session 10 - React - Session 1/task-manager/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"
+import AppW from "./AppW"
+// import App from "./App"
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<AppW/>);
+// ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
  
